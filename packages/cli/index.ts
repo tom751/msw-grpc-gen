3336d565a14aae50d1d03d15114d1d7baa8fee55
Tom Hilton <tomhilton93@gmail.com>
2023-08-02 22:00:30 +0100

    Add output flag

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,27 +1,41 @@
+import { program } from 'commander'
 import fg from 'fast-glob'
 import fs from 'fs'
+import path from 'path'
 import { generate } from './build/generate'
 import { getFilename } from './utils/files'
 
-const fileGlob = process.argv[2]
-if (!fileGlob) {
-  throw new Error('Missing file glob argument')
+program
+  .name('msw-grpc-gen')
+  .description('Generate MSW handlers from protobuf-ts generated client endpoints')
+  .argument('path to client', 'path or glob to protobuf-ts generated clients')
+  .requiredOption('-o, --out <string>', 'path to output generated msw handlers')
+  .parse()
+
+if (program.args.length < 1) {
+  console.log('No path to client specified')
+  process.exit(1)
 }
 
-const serviceFiles = fg.sync(fileGlob)
-if (serviceFiles.length === 0) {
+const clientFiles = fg.sync(program.args[0])
+if (clientFiles.length === 0) {
   console.log('No files found')
   process.exit()
 }
 
-for (const service of serviceFiles) {
-  const output = generate(service)
+interface Options {
+  out: string
+}
+
+const options = program.opts<Options>()
+for (const file of clientFiles) {
+  const output = generate(file)
   if (!output) {
     continue
   }
 
-  // TODO - configure this
-  const mockFilename = `${getFilename(service)}.ts`
-  const outputDir = `../example/${mockFilename}`
+  const mockFilename = `${getFilename(file)}.ts`
+  const outputDir = path.join(__dirname, options.out, mockFilename)
+
   fs.writeFileSync(outputDir, output)
 }
