e2f3f7bb334152c63be3d600326224b33b2bb3f9
Tom Hilton <tomhilton93@gmail.com>
2023-08-12 21:16:19 +0100

    Add proto import example

--- packages/example/src/mocks/gen/example.ts
+++ packages/example/src/mocks/gen/example.ts
@@ -1,17 +1,39 @@
 import { rest } from 'msw';
 import type { CreateUserResponse, User } from '../../../pb/example';
+import type { Address } from '../../../pb/example_dep';
 
 export const handlers = [
     rest.post('/getUser', (req, res, ctx) => {
         return res(ctx.status(200), ctx.json<User>({
             name: '',
             email: '',
-            age: 0
+            age: 0,
+            active: false,
+            amount: 0n,
+            address: {
+                firstLine: '',
+                secondLine: '',
+                town: '',
+                country: '',
+                postCode: ''
+            }
         }));
     }),
     rest.post('/createuser', (req, res, ctx) => {
         return res(ctx.status(200), ctx.json<CreateUserResponse>({
-            message: ''
+            message: '',
+            image: new Uint8Array(),
+            status: 0,
+            users: {}
+        }));
+    }),
+    rest.post('/getUserAddress', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json<Address>({
+            firstLine: '',
+            secondLine: '',
+            town: '',
+            country: '',
+            postCode: ''
         }));
     })
 ];
