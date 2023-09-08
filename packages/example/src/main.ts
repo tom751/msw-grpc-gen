d37b8df64d645173b9a283423630d667c598b905
Tom Hilton <tomhilton93@gmail.com>
2023-09-08 19:29:03 +0100

    Replace rome with biome

--- packages/example/src/main.ts
+++ packages/example/src/main.ts
@@ -20,7 +20,7 @@ const client = new UserServiceClient(transport)
 if (fetchButton) {
   fetchButton.addEventListener('click', () => {
     client.getUser({ name: 'bob' }).then(({ response }) => {
-      // rome-ignore lint/style/noNonNullAssertion: trust me
+      // biome-ignore lint/style/noNonNullAssertion: trust me
       pre!.innerText = JSON.stringify(response, (_k, v) => (typeof v === 'bigint' ? v.toString() : v), 2)
     })
   })
